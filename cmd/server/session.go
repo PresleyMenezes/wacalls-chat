b734fe1c14408cbc825616876c3dd1a3994ba04d
PresleyMenezes <112982462+PresleyMenezes@users.noreply.github.com>
2026-09-10 11:12:10 -0300

    Update session.go

--- cmd/server/session.go
+++ cmd/server/session.go
@@ -322,6 +322,8 @@ func (s *Session) handleEvent(rawEvt any) {
 		}
 	case *events.CallTerminate:
 		callID := callIDFromNode(wrapCall(evt.From, evt.Data))
+		_, foundInReg := s.reg.get(callID)
+		s.log.Info("[DIAG] CallTerminate event received", "call_id", callID, "from", evt.From.String(), "foundInReg", foundInReg)
 		if ac, ok := s.reg.get(callID); ok {
 			s.log.Info("call terminate event", "call_id", callID, "from", evt.From.String())
 			// Avisa o frontend NA HORA (mesmo padrão de quando o próprio
